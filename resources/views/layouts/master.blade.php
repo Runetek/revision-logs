16e43ada37c2486d47d5bdbfb8aed3ddcd917001
Kyle Stevenson <kyle@kylestevenson.me>
2016-03-31 14:11:59 -0700

    add mobile meta tag

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,6 +3,7 @@
 <head>
   <title>RS Revision Logs</title>
   <link rel="stylesheet" type="text/css" href="{{ elixir('css/app.css') }}">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
   <div class="container">
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,6 +4,7 @@
         <title>Revision Logs - Login</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <style>
             html, body {
