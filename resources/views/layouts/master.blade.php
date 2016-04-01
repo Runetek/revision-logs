981aed03259e4ec99092372a66fdb585fc164a47
Kyle Stevenson <kyle@kylestevenson.me>
2016-04-01 11:16:46 -0700

    update page titles for templates and welcome page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>RS Revision Logs</title>
+  <title>Runetek Revision Logs</title>
   <link rel="stylesheet" type="text/css" href="{{ elixir('css/app.css') }}">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 </head>
